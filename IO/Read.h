b5b33b967f917d95d3893b46c32c733b819da246
OTTF <2273682295@qq.com>
2024-08-08 15:52:45 +0800

    For the Beauty of Bits v0.7

--- IO/Read.h
+++ IO/Read.h
@@ -1,14 +1,13 @@
 
 
-#include <cctype>
-#include <cstdio>
-
 template <typename T>
 T read () {
     T res = 0;
 	T ch = getchar();
+	T op = 1;
 
     while (!isdigit (ch) && ch != EOF) {
+		op = (ch == '-' ? -1 : 1);
         ch = getchar ();
 	}
 
@@ -16,5 +15,33 @@ T read () {
         res = (res * 10) + (ch - '0');
         ch = getchar ();
     }
-    return res;
+    return op * res;
 }
+
+template <typename T>
+void read (T& num) {
+    T res = 0;
+	T ch = getchar();
+	T op = 1;
+
+    while (!isdigit (ch) && ch != EOF) {
+		op = (ch == '-' ? -1 : 1);
+        ch = getchar ();
+	}
+
+    while (isdigit (ch)) {
+        res = (res * 10) + (ch - '0');
+        ch = getchar ();
+    }
+    num = op * res;
+}
+
+class Read {
+	public:
+	Read operator>> (auto& other) {
+		read (other);
+		return (*this);
+	}
+};
+
+Read in;
